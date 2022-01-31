68e3d8f527a9e9d27aaabe1cb773a9460aef0deb
adrian delgado zambrano <jodezamb@espol.edu.ec>
2022-01-30 20:00:02 -0500

    Metodo Put, de actualizacion de una dato funcionando correctamente recibe el id para poder actualizarlo

--- routes/user.py
+++ routes/user.py
@@ -1,39 +1,47 @@
-from fastapi import  APIRouter #  DEFINR TOAS LAS RUTAS QUE USAREMOS EN EL ARCHIVO 
-from config.db import  conn  #importar el puntero de la base de datos 
-from schemas.user import userEntity,usersEntity # importando los esquemas desde la car schemas
-from models.user import User
-from passlib.hash import sha256_crypt
-from bson import ObjectId
+# DEFINR TOAS LAS RUTAS QUE USAREMOS EN EL ARCHIVO , response que es una respuesta y status que es una propiedad
+import re
+from fastapi import APIRouter, Response, status
+from config.db import conn  # importar el puntero de la base de datos
+# importando los esquemas desde la car schemas
+from schemas.user import userEntity, usersEntity
+from models.user import User  # para importar el modelo user
+from passlib.hash import sha256_crypt  # Para encriptar la contrasena
+from bson import ObjectId  # Para converitr a un Object ID
+# RESPUESTA DEL SISTEMA SIN CONTENIDO
+from starlette.status import HTTP_204_NO_CONTENT
+
+user = APIRouter()
 
 
-user=APIRouter()
-
-
-#primera ruta
+# primera ruta
 @user.get('/users')
 def find_all_users():
     return usersEntity(conn.local.user.find())
 
+
 @user.post('/users')
 def create_users(user: User):
-    new_user=dict(user)
-    new_user["password"]=sha256_crypt.encrypt(new_user['password'])
+    new_user = dict(user)
+    new_user["password"] = sha256_crypt.encrypt(new_user['password'])
     del new_user["id"]
-    id= conn.local.user.insert_one(new_user).inserted_id
-    user = conn.local.user.find_one({"_id":id})
+    id = conn.local.user.insert_one(new_user).inserted_id
+    user = conn.local.user.find_one({"_id": id})
     return userEntity(user)
-    
+
 
 @user.get('/users/{id}')
 def find_user(id: str):
-    return userEntity(conn.local.user.find_one({"_id":ObjectId(id)}))
-    
+    return userEntity(conn.local.user.find_one({"_id": ObjectId(id)}))
+
 
 @user.put('/users/{id}')
-def update_user():
-    return 'Hellow world'
+def update_user(id: str, user: User):
+    conn.local.user.find_one_and_update(
+        {"_id": ObjectId(id)}, {"$set": dict(user)}
+        )
+    return userEntity(conn.local.user.find_one({"_id":ObjectId(id)}))
 
 @user.delete('/users/{id}')
-def delete_users():
-    return 'Hellow world'
-
+def delete_users(id: str):
+    userEntity(conn.local.user.find_one_and_delete({"_id": ObjectId(id)}))
+    return Response(status_code=HTTP_204_NO_CONTENT)
