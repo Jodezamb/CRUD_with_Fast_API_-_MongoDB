78a490ee7e12a0dc17e4b6b80e26d04b569abb25
adrian delgado zambrano <jodezamb@espol.edu.ec>
2022-01-31 01:26:07 -0500

    Metodos del crud completos, api rest lista para ser consumuda por el fronted

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
+from turtle import title
 from fastapi import FastAPI
 from routes.user import user # importar desde el moduo carptea router y modulo user
 
-app = FastAPI()
+app = FastAPI(
+    title=" REST API with FastApi and MongoDB",
+    description="This is a simple REST API with FastAPI and MongoDB By: Jose Adrian Delgado Zambrano",
+    version="0.0.1",
+    
+    
+
+)
 
 app.include_router(user)
--- routes/user.py
+++ routes/user.py
@@ -9,17 +9,18 @@ from passlib.hash import sha256_crypt  # Para encriptar la contrasena
 from bson import ObjectId  # Para converitr a un Object ID
 # RESPUESTA DEL SISTEMA SIN CONTENIDO
 from starlette.status import HTTP_204_NO_CONTENT
+from typing import List
 
 user = APIRouter()
 
 
 # primera ruta
-@user.get('/users')
+@user.get('/users',response_model=List[User], tags=["users"])
 def find_all_users():
     return usersEntity(conn.local.user.find())
 
 
-@user.post('/users')
+@user.post('/users', response_model=User,tags=["users"])
 def create_users(user: User):
     new_user = dict(user)
     new_user["password"] = sha256_crypt.encrypt(new_user['password'])
@@ -29,19 +30,19 @@ def create_users(user: User):
     return userEntity(user)
 
 
-@user.get('/users/{id}')
+@user.get('/users/{id}',response_model=User,tags=["users"])
 def find_user(id: str):
     return userEntity(conn.local.user.find_one({"_id": ObjectId(id)}))
 
 
-@user.put('/users/{id}')
+@user.put('/users/{id}',response_model=User,tags=["users"])
 def update_user(id: str, user: User):
     conn.local.user.find_one_and_update(
         {"_id": ObjectId(id)}, {"$set": dict(user)}
         )
     return userEntity(conn.local.user.find_one({"_id":ObjectId(id)}))
 
-@user.delete('/users/{id}')
+@user.delete('/users/{id}',status_code=HTTP_204_NO_CONTENT,tags=["users"])
 def delete_users(id: str):
     userEntity(conn.local.user.find_one_and_delete({"_id": ObjectId(id)}))
     return Response(status_code=HTTP_204_NO_CONTENT)
